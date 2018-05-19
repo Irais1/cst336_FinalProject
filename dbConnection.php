c338543f72c04c8e57c8c6008168423eb5d021c5
Irais Gopar <igopar-osorio@csumb.edu>
2018-05-19 04:23:19 +0000

    Adding to heroku

--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,17 @@
 <?php
 function connectToDB() {
 
-$host = 'localhost';
-$db = 'TAM';
-$user = 'Iris';
-$pass = 'cst336';
-$charset = 'utf8mb4';
-
+// $host = 'localhost';
+// $db = 'TAM';
+// $user = 'Iris';
+// $pass = 'cst336';
+// $charset = 'utf8mb4';
+$host = "us-cdbr-iron-east-05.cleardb.net";
+$db = "heroku_c8b34ae9cd193df";
+$username = 'bc1d23cb79635b';
+     //echo "username: $username";
+$password = "e4f61d06";
+    
 //checking whether the URL contains "herokuapp" (using Heroku)
 // if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
 //   $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
--- likes.php
+++ likes.php
@@ -6,7 +6,7 @@ $connect = connectToDB();
 
 //echo $_POST['searchType'];
 //UPDATE `users` SET `username`=[value-1],`password`=[value-2],`id`=[value-3] WHERE 1
-  $sql = "Select * FROM `Likes` WHERE songId = {$_POST['id']}";
+  $sql = "Select * FROM `likes` WHERE songId = {$_POST['id']}";
   $stmt = $connect->prepare($sql);
   $data=array(":password" => $_POST['password']);
   $stmt-> execute();
