b566ba00e35de78fcc83c990ca1400e8a65bf698
Irais Gopar <igopar-osorio@csumb.edu>
2018-05-19 05:53:49 +0000

    userId

--- update.php
+++ update.php
@@ -6,7 +6,7 @@ $connect = connectToDB();
 
 //echo $_POST['searchType'];
 //UPDATE `users` SET `username`=[value-1],`password`=[value-2],`id`=[value-3] WHERE 1
-  $sql = "UPDATE `users` SET `password`= sha1('".$_POST['pass']."') WHERE id = 11";
+  $sql = "UPDATE `users` SET `password`= sha1('".$_POST['pass']."') WHERE userId = 11";
   $stmt = $connect->prepare($sql);
   //$data=array(":password" => $_POST['password']);
   $stmt-> execute();
